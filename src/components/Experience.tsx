3505c9a4f37700b44b48fe171685ac1f61e219b1
Kisal Nelaka <kisalnelaka6@gmail.com>
2026-03-21 08:29:57 +0300

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Briefcase, Calendar } from 'lucide-react';
+import { Briefcase } from 'lucide-react';
 
 const experiences = [
     {
